cf2ebdf1770dbb30f9e0c65dea090fa652bb882a
enzk0 <renzoroel24@gmail.com>
2024-11-21 14:45:52 +0800

    Renamed images to a camel case typing. Also removed spaces between them

---
+++ src/Components/Menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Menu.css';
+
+function Menu() {
+  return (
+    <>
+        <img src={require('../Images/Menu_Banner')} alt="Menu Banner" />
+    </>
+  )
+}
+
+export default Menu
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Menu from '../Components/Menu';
 
 function Home() {
   return (
     <div>
       <h1>This is the Home Page</h1>
+      <Menu />
     </div>
   )
 }
