275ad0b09990655e8e9c85a93963976c9de2f77c
Nutty-Nuts <vegagerard1130@gmail.com>
2024-11-23 16:31:57 +0800

    implemented registering/creating new user account

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,15 +1,94 @@
 import React, { useState, useContext } from "react";
 import { IoMail, IoLockClosed } from "react-icons/io5";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 import { UserContext } from "../App";
 import "../styles/Login.css";
 
 function Register() {
   const [userId, setUserId] = useContext(UserContext);
+  const [registrationInfo, setRegistrationInfo] = useState({
+    email: "",
+    password: "",
+    confirm: "",
+  });
 
-  const handleLogin = () => {
-    setUserId(!userId);
+  const navigate = useNavigate();
+
+  const handleEmailInput = (e) => {
+    setRegistrationInfo({
+      ...registrationInfo,
+      email: e.target.value,
+    });
+  };
+
+  const handlePasswordInput = (e) => {
+    setRegistrationInfo({
+      ...registrationInfo,
+      password: e.target.value,
+    });
+  };
+
+  const handleConfirmInput = (e) => {
+    setRegistrationInfo({
+      ...registrationInfo,
+      confirm: e.target.value,
+    });
+  };
+
+  const handleSubmitRegistrationInfo = () => {
+    if (registrationInfo.password !== registrationInfo.confirm) {
+      alert("Password does not match with Confirm Password");
+      return;
+    }
+
+    if (registrationInfo.email == "") {
+      alert("Please enter a valid email.");
+      return;
+    }
+
+    if (registrationInfo.password == "") {
+      alert("Please enter a password.");
+      return;
+    }
+
+    if (registrationInfo.password == "") {
+      alert("Please enter confirm your password.");
+      return;
+    }
+
+    const user = {
+      username: "User",
+      email: registrationInfo.email,
+      password: registrationInfo.password,
+      contact_no: null,
+      cart: [],
+      orders: [],
+    };
+
+    axios
+      .post(`http://localhost:3000/api/user/register/`, user)
+      .then((res) => {
+        console.log("Request to create new user was successful.");
+      })
+      .catch((error) => {
+        console.warn("An error happened. Please check console");
+        console.error(error);
+      });
+
+    axios
+      .get(
+        `http://localhost:3000/api/user/auth/${registrationInfo.email}/${registrationInfo.password}`,
+      )
+      .then((res) => {
+        setUserId(res.data);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.warn("An error happened. Please check console");
+        console.error(error);
+      });
   };
 
   return (
@@ -24,21 +103,37 @@ function Register() {
           <IoMail />
           <label htmlFor="">Email</label>
         </div>
-        <input className="login-field" type="email" />
+        <input
+          className="login-field"
+          type="email"
+          onChange={(e) => handleEmailInput(e)}
+        />
 
         <div className="login-label">
           <IoLockClosed />
           <label htmlFor="">Password</label>
         </div>
-        <input className="login-field" type="password" />
+        <input
+          className="login-field"
+          type="password"
+          onChange={(e) => handlePasswordInput(e)}
+        />
 
         <div className="login-label">
           <IoLockClosed />
           <label htmlFor="">Confirm Password</label>
         </div>
-        <input className="login-field" type="password" />
+        <input
+          className="login-field"
+          type="password"
+          onChange={(e) => handleConfirmInput(e)}
+        />
 
-        <button className="login-button" type="submit" onClick={handleLogin}>
+        <button
+          className="login-button"
+          type="submit"
+          onClick={handleSubmitRegistrationInfo}
+        >
           <b>Register</b>
         </button>
 
