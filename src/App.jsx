e1f863a003da627f014e0ebdc50f51c1b734b067
Nutty-Nuts <vegagerard1130@gmail.com>
2024-11-23 13:29:37 +0800

    fixed navbar positioning

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import React, {useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import Navbar from './Components/Navbar';
-import Home from './Pages/Home';
-import Order from './Pages/Order';
-import Profile from './Pages/Profile';
-import LoginRegister from './Components/LoginRegister/LoginRegister';
+import React, { createContext, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import Order from "./Pages/Order";
+import Profile from "./Pages/Profile";
+import LoginRegister from "./Components/LoginRegister/LoginRegister";
+
+export const UserContext = createContext();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userId, setUserId] = useState(null);
 
   return (
-    <>
-      <LoginRegister/> 
-        <Navbar />
-        <div className='container'>
-          <Routes>
-            <Route path='/' element={<Home />}/>
-            <Route path='/Order' element={<Order />}/>
-            <Route path='/Profile' element={<Profile />}/>
-          </Routes>
-        </div>
-    </>
-  )
+    <UserContext.Provider value={[userId, setUserId]}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Order" element={<Order />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
+    </UserContext.Provider>
+  );
 }
 
-export default App
+export default App;
