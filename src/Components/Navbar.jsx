aa1530bea1c441f8e61a7d284472e28e7275393e
Nutty-Nuts <vegagerard1130@gmail.com>
2024-11-23 13:32:09 +0800

    changed implementation of navbar to prevent site from reloading whenever the route is changed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,22 +9,22 @@ function Navbar() {
         Catofe
       </a>
       <ul>
-        <CustomLink href="/">Home</CustomLink>
-        <CustomLink href="/Order">Order</CustomLink>
-        <CustomLink href="/Profile">Profile</CustomLink>
+        <Link to="/">Home</Link>
+        <Link to="/Order">Order</Link>
+        <Link to="/Profile">Profile</Link>
       </ul>
     </nav>
   );
 }
 
-function CustomLink({ href, children, ...props }) {
-  const path = window.location.pathname;
-  return (
-    <li className={path === href ? "active" : ""}>
-      <a href={href} {...props}>
-        {children}
-      </a>
-    </li>
-  );
-}
+// function Link({ href, children, ...props }) {
+//   const path = window.location.pathname;
+//   return (
+//     <li className={path === href ? "active" : ""}>
+//       <a href={href} {...props}>
+//         {children}
+//       </a>
+//     </li>
+//   );
+// }
 export default Navbar;
