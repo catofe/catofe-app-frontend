3ac7c51f8e0ae9a92ad65242d6dea7c598d2e606
Nutty-Nuts <vegagerard1130@gmail.com>
2024-12-01 09:28:03 +0800

    redesigned(yassified) the login form/page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from "react";
 import { IoMail, IoLockClosed } from "react-icons/io5";
+import { ImCross } from "react-icons/im";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
@@ -7,7 +8,9 @@ import { UserContext } from "../App";
 import "../styles/Login.css";
 
 function Login() {
+  const inputStyling = "p-2 mb-4 rounded border-2 border-slate-300";
   const [userId, setUserId] = useContext(UserContext);
+  const [incorrect, setIncorrect] = useState(true);
   const [authInfo, setAuthInfo] = useState({
     email: "",
     password: "",
@@ -42,23 +45,33 @@ function Login() {
       .catch((error) => {
         console.warn("An error happened. Please check console");
         console.error(error);
+        setIncorrect(true);
       });
   };
 
   return (
-    <div className="login-page-container">
-      <div className="login-form">
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <div className="p-8 rounded-xl flex flex-col border border-gray-200 shadow-lg bg-white hover:shadow-xl transition-shadow">
         <h2>
           <b>Login</b>
         </h2>
         <p>Login to your catofe account</p>
+        {incorrect ? (
+          <div className="mt-4 p-2 rounded flex flex-row gap-2 justify-center items-center bg-red-100 text-red-500 text-center">
+            <ImCross className="text-sm" />
+            <p>Your email or password is incorrect.</p>
+          </div>
+        ) : (
+          ""
+        )}
 
         <div className="login-label">
           <IoMail />
           <label htmlFor="">Email</label>
         </div>
         <input
-          className="login-field"
+          className={`${inputStyling}`}
+          placeholder="Enter your email here..."
           type="email"
           onChange={(e) => handleEmailInput(e)}
         />
@@ -68,13 +81,14 @@ function Login() {
           <label htmlFor="">Password</label>
         </div>
         <input
-          className="login-field"
+          className={`${inputStyling}`}
+          placeholder="Enter yout password here..."
           type="password"
           onChange={(e) => handlePasswordInput(e)}
         />
 
         <button
-          className="login-button"
+          className="p-4 mt-8 rounded-lg bg-blue-500 text-white tracking-wide hover:bg-blue-600 active:bg-blue-800 transition-colors"
           type="submit"
           onClick={handleSubmitAuthInfo}
         >
@@ -83,7 +97,10 @@ function Login() {
 
         <p className="login-register-link">
           Don't have an account?
-          <Link to="/register">
+          <Link
+            to="/register"
+            className="text-blue-400 hover:text-blue-700 active:text-blue-950 transition-all"
+          >
             <b> Create one here</b>
           </Link>
         </p>
